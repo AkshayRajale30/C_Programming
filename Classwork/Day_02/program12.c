60c339b4e1517feec900e06168ac1693e44dfa72
AkshayRajale30 <akshayrajale1@gmail.com>
2025-10-16 11:56:56 +0530

    Add files via upload

--- Classwork/Day_02/program12.c
+++ Classwork/Day_02/program12.c
@@ -1,1 +1,42 @@
+/*
+   Algorithm
+   START
+        Accespet number and store as no
+        Divide no by 2
+        If the remainder is 0
+           then display as Even
+        otherwise
+           display as Odd  
+    STOP
+*/
 
+#include<stdio.h>
+
+void CheckEvenOdd(int iNo)
+{
+    int iRem = 0;
+
+    iRem = iNo % 2;
+
+    if(iRem == 0)    
+    {
+        printf("It is Even Number\n");
+    }
+    else
+    {
+        printf("It is Odd Number\n");       
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number :");
+    scanf("%d",&iValue);
+
+    CheckEvenOdd(iValue);
+
+
+    return 0;
+}
