0e1711d020ffdbe1db891929a78fb362d3a66249
Akshay Ashok Rajale <akshayrajale1@gmail.com>
2025-10-17 23:05:51 +0530

    Add files via upload

---
+++ Classwork/Day_04/program56.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckPrime(int iNo)
+{
+    int iCnt = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+    
+    // Input : 6  iCnt = 2
+    // Input : 8  iCnt = 2
+    // Input : 12  iCnt = 2
+    // Input : 9  iCnt = 3
+    // Input : 27  iCnt = 3
+
+    // Input : 13  iCnt = 7
+    // Input : 17  iCnt = 9
+    // Input : 23  iCnt = 12
+    // Input : 29  iCnt = 15
+
+    for(iCnt = 2; iCnt <= (iNo / 2); iCnt++)
+    {
+        if((iNo % iCnt) == 0)
+        {
+            break;            // Optimization
+        }
+    }
+
+    if(iCnt > (iNo/2))        // No Factors
+    {
+        return true;
+    }
+    else                      // Atleast one factors
+    {
+        return false;
+    }
+}
+
+int main()
+{ 
+    int iValue = 0;
+    bool bRet = false;
+    
+    printf("Enter the number : \n");
+    scanf("%d",&iValue);
+
+    bRet = CheckPrime(iValue);
+    
+    if(bRet == true)
+    {
+        printf("%d is prime number \n",iValue);
+    }
+    else
+    {
+        printf("%d is not a prime number \n",iValue);
+    }
+
+
+    return 0;
+}
+
+// Time Complexity : Forprime - N/2
+//  Time Complexity : For Non Prime - either 1 or 2
